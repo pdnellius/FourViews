0f7d8f2ed6d9c72de3dd59cfad17a97d45151157
Pete Nellius <peternellius@gmail.com>
2015-03-12 20:28:17 -0400

    Finished app '

---
+++ FourViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourViews
+//
+//  Created by Pete Nellius on 3/12/15.
+//  Copyright (c) 2015 Pete Nellius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
